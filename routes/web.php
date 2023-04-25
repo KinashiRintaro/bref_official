83a0572f2f14794fa8b7beaa90af884f3e597344
KinashiRintaro <kinashi_r@mediaxis.jp>
2023-04-25 15:31:08 +0900

    Laravelが表示できる状態

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     // dd("aa");
     // throw new \Exception("エラーです");
-    abort(404);
+    // abort(404);
     return view('welcome');
 });
